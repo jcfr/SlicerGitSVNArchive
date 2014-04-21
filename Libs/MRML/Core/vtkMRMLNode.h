023a67fb9e8067e81ba4e812c10e83c10ceae38e
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-04-21 13:38:45 -0400

    ENH: Simplify MRMLReferences using vtkSmartPointer
    * Use of smart pointer in vtkMRMLReference allows us to remove
    the complicated code manually managing the calls to Register/UnRegister
    
    * function "SetNthNodeReferenceID" and "SetAndObserveNthNodeReferenceID"
    have been updated to have similar code (at the exception of the
    code setting the observation events).
    
    * SetNthNodeReferenceID/SetAndObserveNthNodeReferenceID have also
    been fixed to work properly in case the reference doesn't exist
    and referencedNodeID is null.

--- Libs/MRML/Core/vtkMRMLNode.h
+++ Libs/MRML/Core/vtkMRMLNode.h
@@ -20,12 +20,13 @@
 #include "vtkObserverManager.h"
 #include "vtkIdTypeArray.h"
 #include "vtkIntArray.h"
-
 class vtkMRMLScene;
 
 
 // VTK includes
 #include <vtkObject.h>
+#include <vtkSmartPointer.h>
+#include <vtkWeakPointer.h>
 class vtkCallbackCommand;
 
 // STD includes
@@ -571,29 +572,22 @@ protected:
     vtkSetStringMacro(ReferencedNodeID);
     vtkGetStringMacro(ReferencedNodeID);
 
-    vtkMRMLNode*    ReferencingNode;
-    vtkMRMLNode*    ReferencedNode;
-    vtkIntArray*    Events;
+    vtkWeakPointer<vtkMRMLNode> ReferencingNode;
+    vtkSmartPointer<vtkMRMLNode> ReferencedNode;
+    vtkSmartPointer<vtkIntArray> Events;
 
   protected:
 
     vtkMRMLNodeReference()
       {
-      this->ReferencedNode = 0;
-      this->ReferencingNode = 0;
-      this->Events = 0;
       this->ReferencedNodeID = 0;
       this->ReferenceRole = 0;
       }
 
     ~vtkMRMLNodeReference()
       {
-      if (this->Events)
-        {
-        this->Events->Delete();
-        this->Events = 0;
-        }
-      };
+      }
+
     vtkMRMLNodeReference(const vtkMRMLNodeReference&);
     void operator=(const vtkMRMLNodeReference&);
 
@@ -604,12 +598,14 @@ protected:
 
   /// NodeReferences is a map that stores vector of refererences for each referenceRole,
   /// the referenceRole can be any unique string, for example "display", "transform" etc.
-  typedef std::map< std::string, std::vector< vtkMRMLNodeReference *> > NodeReferencesType;
+  typedef std::vector< vtkSmartPointer<vtkMRMLNodeReference> > NodeReferenceListType;
+  typedef std::map< std::string, NodeReferenceListType > NodeReferencesType;
   NodeReferencesType NodeReferences;
 
   std::map< std::string, std::string> NodeReferenceMRMLAttributeNames;
 
-  std::map< std::string, vtkIntArray*> NodeReferenceEvents;
+  typedef std::map< std::string, vtkSmartPointer<vtkIntArray> > NodeReferenceEventsType;
+  NodeReferenceEventsType NodeReferenceEvents;
 
 protected:
 
