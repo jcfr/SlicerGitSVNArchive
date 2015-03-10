32f9e46e7b66816bd2b7097ea61eed91ce4ee82a
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-03-09 23:35:18 -0400

    COMP: Add missing vtkAddon.h
    This commit fixes compilation error introduced by
    commit r24039 (ENH: Added vtkInfoMacro)

---
+++ Libs/vtkAddon/vtkAddon.h
@@ -0,0 +1,22 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+==============================================================================*/
+
+#ifndef __vtkAddon_h
+#define __vtkAddon_h
+
+#include "vtkAddonWin32Header.h"
+
+#endif
+
