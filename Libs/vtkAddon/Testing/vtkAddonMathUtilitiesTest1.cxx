fc3bcd7a87886e77f53ca02aa721fd782fd14f1b
Davide <punzodavide@hotmail.it>
2016-05-20 12:24:36 -0400

    ENH : added vtkAddonMathUtilitiesTest1

--- Libs/vtkAddon/Testing/vtkAddonMathUtilitiesTest1.cxx
+++ Libs/vtkAddon/Testing/vtkAddonMathUtilitiesTest1.cxx
@@ -17,12 +17,40 @@
 
 // vtkAddon includes
 #include "vtkAddonMathUtilities.h"
+#include "vtkAddonTestingUtilities.h"
+
+// vtk includes
+#include <vtkNew.h>
+#include <vtkSmartPointer.h>
+#include <vtkMatrix4x4.h>
+
+using namespace vtkAddonTestingUtilities;
 
 //----------------------------------------------------------------------------
 int vtkAddonMathUtilitiesTest1(int vtkNotUsed(argc), char* vtkNotUsed(argv)[])
 {
 
-  // Todo
+  vtkSmartPointer<vtkMatrix4x4> m1 =
+      vtkSmartPointer<vtkMatrix4x4>::New();
+  vtkSmartPointer<vtkMatrix4x4> m2 =
+      vtkSmartPointer<vtkMatrix4x4>::New();
+  vtkSmartPointer<vtkMatrix4x4> m3 =
+      vtkSmartPointer<vtkMatrix4x4>::New();
+  for (int i = 0; i < 4; i++)
+    {
+    for (int j = 0; j < 4; j++)
+      {
+      m1->SetElement(i, j, i * j);
+      m2->SetElement(i, j, i * j);
+      m3->SetElement(i, j, i);
+      }
+    }
 
+  if (!CheckInt(__LINE__, "TestCheckInt", 1, vtkAddonMathUtilities::Matrix4x4AreEqual(m1, m2))
+      || CheckInt(__LINE__, "TestCheckInt Expected Failure", 1, vtkAddonMathUtilities::Matrix4x4AreEqual(m1, m3)))
+    {
+    std::cerr << "Line " << __LINE__ << " - TestCheckInt failed" << std::endl;
+    return EXIT_FAILURE;
+    }
   return EXIT_SUCCESS;
 }
