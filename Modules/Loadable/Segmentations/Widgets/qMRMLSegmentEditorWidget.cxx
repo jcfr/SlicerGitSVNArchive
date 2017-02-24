3bef8bd55c7921c6c629ec9ccc65a20bc83135ee
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-02-23 21:39:29 -0500

    ENH: Add support for Qt5
    Support configuration using either Qt4 or Qt5
    
    Based on preliminary work from Steve Pieper <pieper@bwh.harvard.edu>
    
    vtkSlicerConfigure: Introduced Slicer_HAVE_QT5 and Slicer_HAVE_WEBKIT_SUPPORT.
    This was needed to workaround limitation of "moc" compiler preventing it
    from expanding macro like "QT_VERSION_CHECK.
    
    Tested with Qt 5.7 on Ubuntu
    
    Remaining tasks:
    * Port extension manager and datastore to Qt5
    * Update install/packaging rules to support Qt5
    * SlicerStyle (based on CommonStyle) is disabled (see Main.cxx). Application crashes on Linux.
    * Fix runtime errors:
     * libpng warning: iCCP: known incorrect sRGB profile
     * QPixmap::grabWidget is deprecated, use QWidget::grab() instead

--- Modules/Loadable/Segmentations/Widgets/qMRMLSegmentEditorWidget.cxx
+++ Modules/Loadable/Segmentations/Widgets/qMRMLSegmentEditorWidget.cxx
@@ -81,6 +81,7 @@
 #include <QButtonGroup>
 #include <QMainWindow>
 #include <QMessageBox>
+#include <QPointer>
 #include <QShortcut>
 #include <QVBoxLayout>
 
@@ -101,10 +102,17 @@ public:
     {
     return new vtkSegmentEditorEventCallbackCommand;
     }
+#if (QT_VERSION < QT_VERSION_CHECK(5, 0, 0))
   /// Segment editor widget observing the event
   QWeakPointer<qMRMLSegmentEditorWidget> EditorWidget;
   /// Slice widget or 3D widget
   QWeakPointer<qMRMLWidget> ViewWidget;
+#else
+  /// Segment editor widget observing the event
+  QPointer<qMRMLSegmentEditorWidget> EditorWidget;
+  /// Slice widget or 3D widget
+  QPointer<qMRMLWidget> ViewWidget;
+#endif
 };
 
 //-----------------------------------------------------------------------------
