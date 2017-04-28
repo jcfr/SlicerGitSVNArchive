1ffa49e5cf4d04563a555625cdea0a605dd3167e
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-04-28 19:15:18 -0400

    ENH: Update buildsystem to support packaging of existing extensions
    Co-authored-by: Hina Shah <hina.shah@kitware.com>

---
+++ CMake/SlicerBlockInstallExtensionPackages.cmake
@@ -0,0 +1,13 @@
+
+# Install extension install directories
+foreach(dir IN LISTS Slicer_EXTENSION_INSTALL_DIRS)
+  if(NOT EXISTS ${dir})
+    message(WARNING "Skipping nonexistent extension install directory [${dir}]")
+    continue()
+  endif()
+  install(
+    DIRECTORY "${dir}/"
+    DESTINATION ${Slicer_INSTALL_ROOT}
+    COMPONENT Runtime
+    )
+endforeach()
