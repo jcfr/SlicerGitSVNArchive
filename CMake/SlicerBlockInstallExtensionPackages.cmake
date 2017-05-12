ecce4f83f4ccd707d951dd0a7b902200dcc1933e
Hina Shah <hina.shah@kitware.com>
2017-05-12 13:28:49 -0400

    BUG adding string/list manipulation

--- CMake/SlicerBlockInstallExtensionPackages.cmake
+++ CMake/SlicerBlockInstallExtensionPackages.cmake
@@ -1,5 +1,9 @@
 
+# In case the list was converted to a string with carats, replace them to comply with list convention
+string (REPLACE "^^" ";" Slicer_EXTENSION_INSTALL_DIRS "${Slicer_EXTENSION_INSTALL_DIRS}")
+
 # Install extension install directories
+
 foreach(dir IN LISTS Slicer_EXTENSION_INSTALL_DIRS)
   if(NOT EXISTS ${dir})
     message(WARNING "Skipping nonexistent extension install directory [${dir}]")
