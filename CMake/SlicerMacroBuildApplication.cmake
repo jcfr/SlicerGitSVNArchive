1f9a4e85ad7f89427f4a94e6a0cc552e35af87ef
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2019-04-26 22:34:11 -0400

    COMP: Ensure Slicer_BUILD_WEBENGINE_SUPPORT is consider by moc

--- CMake/SlicerMacroBuildApplication.cmake
+++ CMake/SlicerMacroBuildApplication.cmake
@@ -142,6 +142,9 @@ macro(slicerMacroBuildAppLibrary)
   # Sources
   # --------------------------------------------------------------------------
     set(_moc_options OPTIONS -DSlicer_HAVE_QT5)
+    if(Slicer_BUILD_WEBENGINE_SUPPORT)
+      list(APPEND _moc_options -DSlicer_BUILD_WEBENGINE_SUPPORT)
+    endif()
     QT5_WRAP_CPP(SLICERAPPLIB_MOC_OUTPUT ${SLICERAPPLIB_MOC_SRCS} ${_moc_options})
     QT5_WRAP_UI(SLICERAPPLIB_UI_CXX ${SLICERAPPLIB_UI_SRCS})
     if(DEFINED SLICERAPPLIB_RESOURCES)
--- CMake/SlicerMacroBuildBaseQtLibrary.cmake
+++ CMake/SlicerMacroBuildBaseQtLibrary.cmake
@@ -139,6 +139,9 @@ macro(SlicerMacroBuildBaseQtLibrary)
   # Sources
   # --------------------------------------------------------------------------
     set(_moc_options OPTIONS -DSlicer_HAVE_QT5)
+    if(Slicer_BUILD_WEBENGINE_SUPPORT)
+      list(APPEND _moc_options -DSlicer_BUILD_WEBENGINE_SUPPORT)
+    endif()
     QT5_WRAP_CPP(SLICERQTBASELIB_MOC_OUTPUT ${SLICERQTBASELIB_MOC_SRCS} ${_moc_options})
     QT5_WRAP_UI(SLICERQTBASELIB_UI_CXX ${SLICERQTBASELIB_UI_SRCS})
     if(DEFINED SLICERQTBASELIB_RESOURCES)
